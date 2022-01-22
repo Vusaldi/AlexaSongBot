f5605b36db29e2d4b63df79c12f75a5398d179b3
Vusaldi <93197397+Vusaldi@users.noreply.github.com>
2022-01-22 20:19:42 +0400

    Update song.py

--- AlexaSongBot/modules/song.py
+++ AlexaSongBot/modules/song.py
@@ -28,19 +28,21 @@ async def song(client, message):
     add_chat_to_db(str(chat_id))
     args = get_arg(message) + " " + "song"
     if args.startswith(" "):
-        await message.reply("Enter a song name. Check /help")
+        await message.reply("Mahnı adını daxil edin. Yoxlayın /help")
         return ""
-    status = await message.reply("🔎Searching song from YouTube 📺.. Please wait some time ⏳️ © @Mr_Dark_Prince ")
+    status = await message.reply("🔎YouTube-dan mahnı axtarılır 📺.. Xaiş Edirəm Biraz gözlə⏳️ © @Vusaldeveloper ")
     video_link = yt_search(args)
     if not video_link:
-        await status.edit("😔Song not found.")
+        await status.edit("😔Mahnı tapılmadı
+
+.")
         return ""
     yt = YouTube(video_link)
     audio = yt.streams.filter(only_audio=True).first()
     try:
         download = audio.download(filename=f"{str(user_id)}")
     except Exception as ex:
-        await status.edit("Failed to download song")
+        await status.edit("Mahnı endirmək alınmadı")
         LOGGER.error(ex)
         return ""
     rename = os.rename(download, f"{str(user_id)}.mp3")
